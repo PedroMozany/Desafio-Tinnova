cf896f3c39df030caa1255b5309eb4fa762880d9
unknown <pedro.mozanny@hotmail.com>
2021-11-27 17:55:26 -0300

    Fatorial de um numero natural

--- aplication/Program.java
+++ aplication/Program.java
@@ -5,19 +5,12 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		Fatorial fatorial = new Fatorial();
 		Scanner sc = new Scanner(System.in);
-		BubleSort bolha = new BubleSort();
-		int[] dados = new int[] { 5, 3, 2, 4, 7, 1, 0, 6 };
+		System.out.println("Digite um valor: ");
+		int numeroNatural = sc.nextInt();
 
-		System.out.print("Deseja ver passo a passo S/N: ");
-
-		char resp = sc.next().charAt(0);
-
-		if ((resp == 'S') || (resp == 's')) {
-			System.out.println("Resultado: " + bolha.bolha(dados, true));
-		} else if ((resp == 'N') || (resp == 'n')) {
-			System.out.println("Resultado: " + bolha.bolha(dados, false));
-		}
+		System.out.println("Resultado: " + fatorial.fatorial(numeroNatural));
 
 		sc.close();
 	}
